306f4415705a5b2c9d104b6765480b1d306a31dc
javedkasim <javed78khan28@gmail.com>
2025-09-26 18:06:21 +0530

    third commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logoImage from "../public/img/Home/logo.png"
+import logoImage from "../../public/img/Home/logo.png"
 
 
 const Navbar = () => {
