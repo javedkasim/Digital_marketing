a1b26286ba7e1421e61cb69c08d9825582143cfd
javedkasim <javed78khan28@gmail.com>
2025-09-26 17:10:08 +0530

    third commit

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import bannerImage from "@/public/img/Home/banner-img.png"
+import bannerImage from "../public/img/Home/banner-img.png"
 
 const Banner = () => {
   return (
